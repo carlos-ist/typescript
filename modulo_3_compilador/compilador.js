6cb41226cc1784d3d86325d54ebc0b56b067c1e7
jose-ist <carlos.work@outlook.com>
2022-06-02 17:42:15 -0300

    Módulo 3 - Compilador - Source Map e noEmitOnError: true

---
+++ modulo_3_compilador/compilador.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("From ./modulo_3_compilador/compilador.ts");
+// Aulas sobre o "tsconfig.json"
+let canal = 'gaveta';
+let inscritos = 600000;
+//canal = inscritos
+console.log('');
+console.log(" Propriedade: noEmitOnError: true,");
+console.log(" Não permite a transpilação em javascript caso haja erro no Tyoescript");
+console.log('');
+// propriedade source map
+//# sourceMappingURL=compilador.js.map
---
+++ modulo_3_compilador/compilador.ts
@@ -0,0 +1,19 @@
+console.log("From ./modulo_3_compilador/compilador.ts")
+
+// Aulas sobre o "tsconfig.json"
+
+let canal: string = 'gaveta'
+let inscritos: number = 600000
+
+//canal = inscritos
+
+console.log('')
+console.log(" Propriedade: noEmitOnError: true,")
+console.log(" Não permite a transpilação em javascript caso haja erro no Tyoescript")
+console.log('')
+
+// propriedade source map
+
+
+
+
