3c2cab4c6eb4736ec25522deaf1bd92dbf6c4e58
jose-ist <carlos.work@outlook.com>
2022-06-07 08:55:53 -0300

    Módulo 5 - Classe - Desafio

--- modulo_5_classes/classes.js
+++ modulo_5_classes/classes.js
@@ -14,4 +14,35 @@ console.log(aniversario);
 const casamento = new Data; // posso omitir os "()"
 casamento.ano = 2017;
 console.log(casamento);
+class DataEsperta {
+    constructor(dia = 1, mes = 1, ano = 1970) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+}
+const aniversarioEsperto = new DataEsperta(3, 11, 1991);
+console.log(aniversarioEsperto);
+const casamentoEsperto = new DataEsperta; // posso omitir os "()"
+casamentoEsperto.ano = 2017;
+console.log(casamentoEsperto);
+/*
+Desafio Produto
+Atributos: nome, preco e desconto.
+Criar o construtor
+Obs 1.: Desconto é opcional (valor padrão 0)
+Obs 2.: Criar dois produtos: passando 2 e 3 params
+*/
+console.log('');
+class Produto {
+    constructor(nome, preco, desconto = 0) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+}
+let manteiga = new Produto("Qualy", 6);
+console.log(manteiga);
+let geladeira = new Produto("Brastemp", 3000, (3000 * 0.1));
+console.log(geladeira);
 //# sourceMappingURL=classes.js.map
--- modulo_5_classes/classes.ts
+++ modulo_5_classes/classes.ts
@@ -25,4 +25,47 @@ console.log(casamento)
 
 
 
+class DataEsperta {
+
+    constructor(
+        public dia: number = 1, 
+        public mes: number = 1, 
+        public ano: number = 1970 ) {
+    
+    }
+
+}
+
+const aniversarioEsperto = new DataEsperta(3,11,1991)
+console.log(aniversarioEsperto)
+
+const casamentoEsperto = new DataEsperta // posso omitir os "()"
+casamentoEsperto.ano = 2017
+console.log(casamentoEsperto)
+
+/* 
+Desafio Produto
+Atributos: nome, preco e desconto.
+Criar o construtor
+Obs 1.: Desconto é opcional (valor padrão 0)
+Obs 2.: Criar dois produtos: passando 2 e 3 params
+*/
+
+console.log('')
+
+class Produto {
+    constructor(
+        public nome: string,
+        public preco: number,
+        public desconto: number = 0,
+    ) {}
+}
+
+let manteiga = new Produto("Qualy", 6)
+console.log(manteiga)
+
+let geladeira = new Produto("Brastemp", 3000, (3000*0.1))
+console.log(geladeira)
+
+
 
