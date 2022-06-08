d8a8d9c5700389de0c01cc47831947e2b58d248f
jose-ist <carlos.work@outlook.com>
2022-06-08 18:14:55 -0300

    Módulo 6 - Módulos e Namespaces - Export Import SystemJS

--- modulo_6_modulos_e_namespaces_em_typescript/modulos/modulos.js
+++ modulo_6_modulos_e_namespaces_em_typescript/modulos/modulos.js
@@ -3,6 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 console.log('From ./modulo_6_modulos_e_namespaces_em_typescript/modulos/modulos.js');
 const retangulo_1 = require("./retangulo");
 const circunferencia_1 = require("./circunferencia");
+console.log('');
+console.log("Módulo Carregado...");
 console.log((0, retangulo_1.areaRetangulo)(7, 8));
 console.log((0, circunferencia_1.areaCircunferencia)(2));
 //# sourceMappingURL=modulos.js.map
--- modulo_6_modulos_e_namespaces_em_typescript/modulos/modulos.ts
+++ modulo_6_modulos_e_namespaces_em_typescript/modulos/modulos.ts
@@ -3,5 +3,7 @@ console.log('From ./modulo_6_modulos_e_namespaces_em_typescript/modulos/modulos.
 import { areaRetangulo } from "./retangulo"
 import { areaCircunferencia } from "./circunferencia"
 
+console.log('')
+console.log("Módulo Carregado...")
 console.log(areaRetangulo(7,8))
 console.log(areaCircunferencia(2))
