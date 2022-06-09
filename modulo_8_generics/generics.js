0d769760d967f21e61844ec2378a55a2693a4529
jose-ist <carlos.work@outlook.com>
2022-06-09 18:55:33 -0300

    Módulo 8 - Generics - Classes com Generics

--- modulo_8_generics/generics.js
+++ modulo_8_generics/generics.js
@@ -20,4 +20,38 @@ const avaliacoes = [10, 9.3, 7.7];
 avaliacoes.push(8.4);
 //avaliacoes.push('5.5')
 console.log(avaliacoes);
+// Array
+function imprimir(args) {
+    args.forEach(elemento => console.log(elemento));
+}
+imprimir([1, 2, 3]);
+imprimir([1, 2, 3]);
+imprimir(['Alpha', 'Beta', 'Gama']);
+imprimir([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 }
+]);
+console.log('');
+imprimir([
+    { nome: 'Fulano', idade: 22 },
+    { nome: 'Cicrano', idade: 23 },
+    { nome: 'Beltrano', idade: 24 }
+]);
+const chamarEcho = echoMelhorado; //Função declarada acima
+console.log(chamarEcho('Alguma coisa'));
+// Classes com Generics
+class OperacaoBinaria {
+    constructor(operando1, operando2) {
+        this.operando1 = operando1;
+        this.operando2 = operando2;
+    }
+    executar() {
+        return this.operando1 + this.operando2;
+    }
+}
+console.log(new OperacaoBinaria('Bom', 'dia').executar());
+console.log(new OperacaoBinaria(3, 7).executar());
+console.log(new OperacaoBinaria(3, 'Opa').executar());
+console.log(new OperacaoBinaria({}, {}).executar());
 //# sourceMappingURL=generics.js.map
--- modulo_8_generics/generics.ts
+++ modulo_8_generics/generics.ts
@@ -27,5 +27,53 @@ avaliacoes.push(8.4)
 //avaliacoes.push('5.5')
 console.log(avaliacoes)
 
+// Array
+function imprimir<T>(args:T[]){
+    args.forEach(elemento=>console.log(elemento))
+}
+
+imprimir([1, 2, 3])
+imprimir<number>([1, 2, 3])
+imprimir<string>(['Alpha', 'Beta', 'Gama'])
+
+imprimir<{nome: string, idade: number}>([
+    {nome:'Fulano', idade:22},
+    {nome:'Cicrano', idade:23},
+    {nome:'Beltrano', idade: 24}
+])
+
+console.log('')
+type Aluno = {nome: string, idade: number}
+imprimir<Aluno>([
+    {nome:'Fulano', idade:22},
+    {nome:'Cicrano', idade:23},
+    {nome:'Beltrano', idade: 24}
+])
+
+// Tipo Genérico
+type Echo = <T>(data:T) => T // Essa definição lembra uma interface, um protocolo de uso.
+const chamarEcho: Echo = echoMelhorado //Função declarada acima
+console.log(chamarEcho<string>('Alguma coisa'))
+
+
+// Classes com Generics
+class OperacaoBinaria{
+    constructor(
+        public operando1: any,
+        public operando2: any 
+        ){}
+
+    executar(){
+        return this.operando1 + this.operando2
+    }
+}
+
+console.log(new OperacaoBinaria('Bom', 'dia').executar())
+console.log(new OperacaoBinaria(3, 7).executar())
+console.log(new OperacaoBinaria(3, 'Opa').executar())
+console.log(new OperacaoBinaria({}, {}).executar())
+
+
+
 
 
