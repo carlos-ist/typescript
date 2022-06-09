a9c0d6c92b92bd972b7117f7b0bd98154df52bfe
jose-ist <carlos.work@outlook.com>
2022-06-09 11:08:19 -0300

    Módulo 7 - Interfaces - Interface para Função

--- modulo_7_interfaces/interfaces.js
+++ modulo_7_interfaces/interfaces.js
@@ -7,6 +7,7 @@ function saudarComOla(pessoa) {
 function mudarNome(pessoa) {
     pessoa.nome = 'Joana';
 }
+// Object Literals
 const pessoa = {
     nome: 'João',
     idade: 27,
@@ -19,4 +20,27 @@ saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
 pessoa.saudar('Skywalker');
+// Usando Classes...
+class Cliente {
+    constructor() {
+        this.nome = '';
+        this.ultimaCompra = new Date;
+    }
+    saudar(sobrenome) {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome);
+    }
+}
+const meuCliente = new Cliente;
+meuCliente.nome = 'Han';
+saudarComOla(meuCliente);
+meuCliente.saudar('Solo');
+console.log(meuCliente.ultimaCompra);
+let potencia;
+potencia = function (base, exp) {
+    // Math.pow (3,10)
+    // 3 ** 10
+    // Array(exp).fill(base).reduce((t, a) => t * a)
+    return Math.pow(base, exp);
+};
 //# sourceMappingURL=interfaces.js.map
--- modulo_7_interfaces/interfaces.ts
+++ modulo_7_interfaces/interfaces.ts
@@ -17,6 +17,7 @@ function mudarNome(pessoa: Humano) {
     pessoa.nome='Joana'
 }
 
+// Object Literals
 const pessoa: Humano = {
     nome: 'João',
     idade: 27,
@@ -32,5 +33,37 @@ saudarComOla(pessoa)
 mudarNome(pessoa)
 saudarComOla(pessoa)
 pessoa.saudar('Skywalker')
+
+
+// Usando Classes...
+class Cliente implements Humano {
+    nome: string = ''
+    ultimaCompra: Date = new Date
+    saudar(sobrenome: string) :void {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome)
+    }
+}
+
+const meuCliente = new Cliente
+meuCliente.nome = 'Han'
+saudarComOla(meuCliente)
+meuCliente.saudar('Solo')
+console.log(meuCliente.ultimaCompra)
+
+
+// Interface Função
+interface FuncaoCalculo {
+    (a: number, b: number) : number
+}
+
+let potencia: FuncaoCalculo
+
+potencia = function (base: number, exp: number): number {
+    // Math.pow (3,10)
+    // 3 ** 10
+    // Array(exp).fill(base).reduce((t, a) => t * a)
+    return base ** exp
+}
 
 
