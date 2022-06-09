02b04ff11c19d0c574bf2be707597b909a464db5
jose-ist <carlos.work@outlook.com>
2022-06-09 10:32:20 -0300

    Módulo 7 - Interfaces - Declaração, Atributos e Métodos Opcionais

--- modulo_7_interfaces/interfaces.js
+++ modulo_7_interfaces/interfaces.js
@@ -4,9 +4,19 @@ console.log('');
 function saudarComOla(pessoa) {
     console.log('Olá, ' + pessoa.nome);
 }
+function mudarNome(pessoa) {
+    pessoa.nome = 'Joana';
+}
 const pessoa = {
     nome: 'João',
-    idade: 27
+    idade: 27,
+    saudar(sobrenome) {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome);
+    }
 };
 saudarComOla(pessoa);
+mudarNome(pessoa);
+saudarComOla(pessoa);
+pessoa.saudar('Skywalker');
 //# sourceMappingURL=interfaces.js.map
--- modulo_7_interfaces/interfaces.ts
+++ modulo_7_interfaces/interfaces.ts
@@ -1,15 +1,36 @@
 console.log('From ./modulo_7_interfaces/interfaces.js')
 console.log('')
 
-function saudarComOla(pessoa: {nome:string}) {
+interface Humano {
+    nome: string
+    idade?: number // Atributo opcional
+    ["abc"]?: true   // Atributo opcional, atributo dinâmico, com propriedade ainda a ser definida
+    saudar(sobrenome: string): void
+    //saudar?(sobrenome: string): void //O método também pode ser opcional na interface, colocando o interrogação ao lado do nome da função.
+}
+
+function saudarComOla(pessoa: Humano) {
     console.log('Olá, ' + pessoa.nome)
 }
 
-const pessoa = {
+function mudarNome(pessoa: Humano) {
+    pessoa.nome='Joana'
+}
+
+const pessoa: Humano = {
     nome: 'João',
-    idade: 27
+    idade: 27,
+
+    saudar(sobrenome: string): void {
+        console.log('Olá, meu nome é '
+            + this.nome + ' ' + sobrenome)
+    }
+
 }
 
 saudarComOla(pessoa)
+mudarNome(pessoa)
+saudarComOla(pessoa)
+pessoa.saudar('Skywalker')
 
 
