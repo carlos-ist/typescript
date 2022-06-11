f4b85f8e0dff259f672cbe391711f4be3ffe50c3
carlos-ist <carlos.work@outlook.com>
2022-06-11 17:03:58 -0300

    Módulo 9 - Decorators

--- modulo_9_decorators/decorators.js
+++ modulo_9_decorators/decorators.js
@@ -6,11 +6,32 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 console.log('From ./modulo_9_decorators/decorators.js');
-let Eletrodomestico = class Eletrodomestico {
+/*
+Nota: Decorator é chamado apenas quando a classe é carregada.
+Ainda que se instancie objetos, sua execução ocorre na chamada/declaração da classe.
+E não durante a instanciação dos objetos.
+*/
+//@logarClasse
+let Eletrodomestico = 
+// Quando uma função retorna um decorator, esse tipo de funão chama-se "Factory"
+class Eletrodomestico {
+    constructor(dado2) {
+        //dado=1
+        dado2 = 's';
+    }
 };
 Eletrodomestico = __decorate([
-    logarClasse
+    logarClasseSe(true) //este decorator retorna e chama o decorator @logarClasse
+    // Quando uma função retorna um decorator, esse tipo de funão chama-se "Factory"
 ], Eletrodomestico);
-console.log(typeof Eletrodomestico);
-function logarClasse() { }
+//console.log(typeof Eletrodomestico)
+function logarClasse(constructor) {
+    console.log(constructor);
+}
+//retornando decorator vazio, pois retornar Null ocasiona erro compilação no typescript
+function decoratorVazio(_) { }
+// Factory
+function logarClasseSe(valor) {
+    return valor ? logarClasse : decoratorVazio;
+}
 //# sourceMappingURL=decorators.js.map
--- modulo_9_decorators/decorators.ts
+++ modulo_9_decorators/decorators.ts
@@ -1,24 +1,42 @@
 console.log('From ./modulo_9_decorators/decorators.js')
 
-@logarClasse
+/*
+Nota: Decorator é chamado apenas quando a classe é carregada.
+Ainda que se instancie objetos, sua execução ocorre na chamada/declaração da classe.
+E não durante a instanciação dos objetos.
+*/
+//@logarClasse
+@logarClasseSe(true) //este decorator retorna e chama o decorator @logarClasse
+// Quando uma função retorna um decorator, esse tipo de funão chama-se "Factory"
 class Eletrodomestico {
-    constructor(){
-        console.log('novo...')
+
+    static dado:number
+
+    constructor(
+        
+        dado2:string
+    ){
+        
+        //dado=1
+        dado2='s'
     }
 
 }
 
 //console.log(typeof Eletrodomestico)
 
-function logarClasse(constructor:Function){
+function logarClasse(constructor: Function): void{
     console.log(constructor)
 }
 
+//retornando decorator vazio, pois retornar Null ocasiona erro compilação no typescript
+function decoratorVazio( _:Function){}
 
 
-
-
-
+// Factory
+function logarClasseSe(valor:boolean){
+    return valor ? logarClasse : decoratorVazio
+}
 
 
 
