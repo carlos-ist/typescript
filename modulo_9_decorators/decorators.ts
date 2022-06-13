2a295d4ee15ac1f075f55799784cf9c64565bc48
jose-ist <carlos.work@outlook.com>
2022-06-13 11:06:15 -0300

    Módulo 9 - Decorators - Introcução

--- modulo_9_decorators/decorators.ts
+++ modulo_9_decorators/decorators.ts
@@ -5,39 +5,34 @@ Nota: Decorator é chamado apenas quando a classe é carregada.
 Ainda que se instancie objetos, sua execução ocorre na chamada/declaração da classe.
 E não durante a instanciação dos objetos.
 */
+
 //@logarClasse
-@logarClasseSe(true) //este decorator retorna e chama o decorator @logarClasse
-// Quando uma função retorna um decorator, esse tipo de funão chama-se "Factory"
+//@logarClasseSe(true) //factory - retorna um decorator
+@decorator({a: 'Teste', b: 123}) //Factory que retorna um decorator, imprimi teste ao receber a classe.
 class Eletrodomestico {
-
-    static dado:number
-
-    constructor(
-        
-        dado2:string
-    ){
-        
-        //dado=1
-        dado2='s'
+    constructor(){
+        console.log('novo...')
     }
-
 }
 
-//console.log(typeof Eletrodomestico)
+//console.log(typeof Eletrodomestico) // apenas comprovando que uma classe em javascript é (retorna) uma function
 
-function logarClasse(constructor: Function): void{
+function logarClasse(constructor:Function){
     console.log(constructor)
 }
 
-//retornando decorator vazio, pois retornar Null ocasiona erro compilação no typescript
-function decoratorVazio( _:Function){}
+function decoratorVazio(_:Function){} //Recebe uum parâmetri do tipo "function" (a classe) e não retorna nada...
 
 
-// Factory
 function logarClasseSe(valor:boolean){
     return valor ? logarClasse : decoratorVazio
 }
 
+function decorator(obj: {a: string, b?: number}){
+    return function(_:Function):void {
+        console.log(obj.a + ' ' + obj.b)
+    }
+}
 
 
 
