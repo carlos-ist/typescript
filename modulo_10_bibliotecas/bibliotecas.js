4a238fd7a7240d1038bdf7020365b868bd96a747
jose-ist <carlos.work@outlook.com>
2022-06-14 16:29:27 -0300

    Módulo 10 - Bibliotecas - jQuery

---
+++ modulo_10_bibliotecas/bibliotecas.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log('From ./modulo_10_bibliotecas/bibliotecas.js');
+$('body').append('Usando jQuery');
+//# sourceMappingURL=bibliotecas.js.map
---
+++ modulo_10_bibliotecas/bibliotecas.ts
@@ -0,0 +1,9 @@
+console.log('From ./modulo_10_bibliotecas/bibliotecas.js')
+
+$('body').append('Usando jQuery')
+
+
+
+
+
+
